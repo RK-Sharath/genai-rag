210d8793a04f8d3c41a9ec3251222c24a6038f94
R K Sharath Kumar <sharathk.rk@gmail.com>
2023-07-24 00:15:41 +0530

    Update RAG-Demo.py

--- RAG-Demo.py
+++ RAG-Demo.py
@@ -19,6 +19,7 @@ from langchain.vectorstores import Chroma
 # Page title
 st.title('🦜🔗 Retriever Augmented Generation Demo powered by IBM Watsonx')
 st.caption("This demo is prepared by Sharath Kumar RK, Senior Data Scientist, Watsonx team")
+st.subheader("Ask questions about your document")
 
 genai_api_key = st.sidebar.text_input("GenAI API Key", type="password")
 genai_api_url = st.sidebar.text_input("GenAI API URL", type="default")
